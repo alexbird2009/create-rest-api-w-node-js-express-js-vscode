ca24bd56587b348064ed2a5267087a36a681fb08
Alex <56090574+alexbird2009@users.noreply.github.com>
2021-01-10 03:11:01 +0200

    last commit

--- routes/users.js
+++ routes/users.js
@@ -2,49 +2,20 @@ import express from 'express';
 
 const router = express.Router();
 
-import { v4 as uuidv4 } from 'uuid';
-
-let users = [];
+import { createUser, getUsers, getUserById, deleteUserById, updateUserById } from '../controllers/users.js'
 
 // all routes in here are starting with /users
-router.get('/', (req, res) => {
-    
-    res.send(users);
 
-});
+router.get('/', getUsers);
 
-router.post('/', (req, res) => {
-    
-    const user = req.body;
-
-    users.push({ ...user, id: uuidv4() });
-
-    res.send(`User ${user.firstName} added to database!`);
-})
+router.post('/', createUser)
 
 // /users/2 => req.params {id: 2}
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
+router.get('/:id', getUserById);
 
-    const foundedUser = users.find((user) => user.id == id );
-    
-    res.send(foundedUser);
-});
+router.delete('/:id', deleteUserById);
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    users = users.filter((user) => user.id !== id);
-
-    res.send(`User ${ id } deleted from the database`)
-})
-
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-
-    const userToBeUpdated = users.find((user) => user.id == id);
-
-});
+router.patch('/:id', updateUserById);
 
 export default router;
